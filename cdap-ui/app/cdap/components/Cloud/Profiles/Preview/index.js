ae6a513eee3df41216c06455ae14e35a689e6ff8
Ajai <ajainarayanan@gmail.com>
2018-04-02 17:23:17 -0700

    Initial version of profile preview in pipeline detailed view for historical runs

---
+++ cdap-ui/app/cdap/components/Cloud/Profiles/Preview/index.js
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2018 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+*/
+
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {MyProfileApi} from 'api/cloud';
+import {getCurrentNamespace} from 'services/NamespaceStore';
+import LoadingSVG from 'components/LoadingSVG';
+
+export default class ProfilePreview extends Component {
+  static propTypes = {
+    profileName: PropTypes.string,
+    profileCustomProperties: PropTypes.object
+  };
+  state = {
+    profileDetails: null,
+    loading: true,
+    error: null
+  };
+
+  componentDidMount() {
+    MyProfileApi.get({
+      namespace: getCurrentNamespace(),
+      profile: this.props.profileName
+    })
+    .subscribe(
+      profileDetails => {
+        this.setState({
+          profileDetails,
+          loading: false
+        });
+      },
+      error => {
+        this.setState({
+          error,
+          loading: false
+        });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <div className="profile-preview">
+          <LoadingSVG />
+        </div>
+      );
+    }
+    return (
+      <div className="profile-preview">
+        <strong>{this.props.profileName}</strong>
+        {JSON.stringify(this.state.profileDetails, null, 2)}
+      </div>
+    );
+  }
+}
